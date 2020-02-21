00dddd0916368fe87b1ec44380d287c438f23c33
Hamzahmaidi <hamza.hmaidi@insat.u-carthage.tn>
2020-02-21 21:43:50 +0100

    modifier main

--- MyAppAWT.java
+++ MyAppAWT.java
@@ -2,7 +2,7 @@ public class MyAppAWT
 {
 public static void main (String args [])
 {
-System.out.println("Application AWT");
+System.out.println("Application AWT Lancée");
 MyFrameAWT f = new MyFrameAWT();
 }
 }
